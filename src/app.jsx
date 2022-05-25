5c33af40e735546d58f54f87e00be94ff77f086b
Yagnesh Modh <hey@yagneshmodh.com>
2022-05-25 17:14:25 +0530

    commit code

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl font-bold text-red-500">
+        Hello from h1
+      </h1>
+    </div>
+  );
+}
+
+export default App;
