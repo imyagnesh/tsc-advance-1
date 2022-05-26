50d316c7d312facad84bcef1895362de8c4aa5e9
Yagnesh Modh <hey@yagneshmodh.com>
2022-05-26 16:47:01 +0530

    commit example of counter

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1 className="text-6xl font-bold text-red-500">
-        Hello from h1
-      </h1>
-    </div>
-  );
+// Function Component
+// function App() {
+//   return (
+//     <div>
+//       <h1 className="text-6xl font-bold text-red-500">
+//         Hello from h1
+//       </h1>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  state = {
+    counter: 0,
+  };
+
+  incrementCounter = () => {
+    // const { counter } = this.state;
+    // this.setState({
+    //   counter: counter + 1,
+    // });
+
+    // this.setState({ counter: this.state.counter + 1 });
+
+    this.setState(({ counter }) => ({
+      counter: counter + 1,
+    }));
+
+    // this.setState(({ counter }) => ({
+    //   counter: counter + 1,
+    // }));
+  };
+
+  render() {
+    console.log('render');
+    const { counter } = this.state;
+
+    return (
+      <div>
+        <h1 className="text-6xl font-bold text-red-500">
+          Hello from h1
+        </h1>
+        <h2>{`Counter: ${counter}`}</h2>
+        <button
+          type="button"
+          onClick={this.incrementCounter}>
+          Increment Counter
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -6,4 +6,4 @@ import './style.css';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App text="hello text" color="green" />);
+root.render(<App name="yagnesh" />);
