fbf878ed117f413f17bcbed9cd02cc36eff5ab8b
Yagnesh Modh <hey@yagneshmodh.com>
2022-05-31 19:02:49 +0530

    completed basic todo app

--- src/app.jsx
+++ src/app.jsx
@@ -1,60 +1,183 @@
 import React, { Component, createRef } from 'react';
+import cn from 'classnames';
+import { Formik } from 'formik';
+// import { FixedSizeList as List } from 'react-window';
 
 class Todo extends Component {
   state = {
     todoList: [],
+    filterType: 'all',
   };
 
-  inputRef = createRef();
-
-  addTodo = event => {
-    event.preventDefault();
-    const todoText = this.inputRef.current;
+  addTodo = (values, actions) => {
     this.setState(
       ({ todoList }) => ({
         todoList: [
           ...todoList,
           {
             id: new Date().valueOf(),
-            text: todoText.value,
+            text: values.todoText,
+            isDone: false,
           },
         ],
       }),
-      () => (todoText.value = '')
+      () => {
+        actions.resetForm();
+      }
     );
   };
 
+  toggleComplete = id => {
+    this.setState(({ todoList }) => {
+      const index = todoList.findIndex(
+        item => item.id === id
+      );
+      return {
+        todoList: [
+          ...todoList.slice(0, index),
+          {
+            ...todoList[index],
+            isDone: !todoList[index].isDone,
+          },
+          ...todoList.slice(index + 1),
+        ],
+      };
+    });
+  };
+
+  deleteTodo = id => {
+    this.setState(({ todoList }) => {
+      const index = todoList.findIndex(
+        item => item.id === id
+      );
+      return {
+        todoList: [
+          ...todoList.slice(0, index),
+          ...todoList.slice(index + 1),
+        ],
+      };
+    });
+  };
+
+  filter = filterType => {
+    this.setState({ filterType });
+  };
+
   render() {
-    console.log('render');
-    const { todoList } = this.state;
+    const { todoList, filterType } = this.state;
 
     return (
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center bg-slate-100 h-screen">
         <h1>Todo Application</h1>
-        <form className="flex my-4" onSubmit={this.addTodo}>
-          <input
-            ref={this.inputRef}
-            type="text"
-            placeholder="Please Enter your todo here..."
-          />
+        <Formik
+          initialValues={{
+            todoText: '',
+          }}
+          onSubmit={this.addTodo}
+          validate={values => {
+            const errors = {};
+            if (!values.todoText.trim()) {
+              errors.todoText = 'required...';
+            }
+            return errors;
+          }}>
+          {({
+            values,
+            handleChange,
+            handleSubmit,
+            handleBlur,
+            errors,
+            isValid,
+            dirty,
+            isSubmitting,
+          }) => (
+            <form
+              onSubmit={handleSubmit}
+              className="flex flex-col justify-center">
+              <div>
+                <input
+                  value={values.todoText}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  type="text"
+                  name="todoText"
+                  placeholder="Please Enter your todo here..."
+                />
+                <button
+                  disabled={
+                    !dirty || !isValid || isSubmitting
+                  }
+                  className="btn rounded-l-none disabled:bg-slate-400 disabled:cursor-not-allowed"
+                  type="submit">
+                  Add Todo
+                </button>
+              </div>
+              {errors.todoText && (
+                <span className="text-red-400">
+                  {errors.todoText}
+                </span>
+              )}
+            </form>
+          )}
+        </Formik>
+        <div className="w-full max-w-5xl flex-1 overflow-y-auto">
+          {todoList.map(item => {
+            const listItem = (
+              <div
+                key={item.id}
+                className="flex m-4 items-center">
+                <input
+                  type="checkbox"
+                  checked={item.isDone}
+                  onChange={() =>
+                    this.toggleComplete(item.id)
+                  }
+                />
+                <p
+                  className={cn('flex-1 px-4', {
+                    'line-through': item.isDone,
+                  })}>
+                  {item.text}
+                </p>
+                <button
+                  type="button"
+                  className="btn bg-red-400"
+                  onClick={() => this.deleteTodo(item.id)}>
+                  Delete
+                </button>
+              </div>
+            );
+
+            if (
+              (filterType === 'completed' && item.isDone) ||
+              (filterType === 'pending' && !item.isDone) ||
+              filterType === 'all'
+            ) {
+              return listItem;
+            }
+
+            return null;
+          })}
+        </div>
+        <div className="w-full flex">
           <button
-            className="btn rounded-l-none"
-            type="submit">
-            Add Todo
+            type="button"
+            className="btn flex-1 rounded-none"
+            onClick={() => this.filter('all')}>
+            All
           </button>
-        </form>
-        <div className="w-full max-w-5xl">
-          {todoList.map((item, index) => (
-            <div className="flex m-4 items-center">
-              <input type="checkbox" />
-              <p className="flex-1 px-4">{item.text}</p>
-              <button
-                type="button"
-                className="btn bg-red-400">
-                Delete
-              </button>
-            </div>
-          ))}
+          <button
+            type="button"
+            className="btn flex-1 rounded-none"
+            onClick={() => this.filter('pending')}>
+            Pending
+          </button>
+          <button
+            type="button"
+            className="btn flex-1 rounded-none"
+            onClick={() => this.filter('completed')}>
+            Completed
+          </button>
         </div>
       </div>
     );
